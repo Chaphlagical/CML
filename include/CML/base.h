48e0649da2be98bc2ebfdbf40891e3cdee4a288c
Chaf Chen <35370033+Chaphlagical@users.noreply.github.com>
2020-10-11 11:44:04 +0800

    add namespace

--- include/CML/base.h
+++ include/CML/base.h
@@ -3,23 +3,27 @@
 
 namespace Chaf
 {
-	template<typename T>
-	constexpr auto PI = static_cast<T>(3.1415926535);
+	namespace cml
+	{
+			template<typename T>
+			constexpr auto PI = static_cast<T>(3.1415926535);
 
-	template<typename T>
-	constexpr auto EPSILON = static_cast<T>(0.00001);
+			template<typename T>
+			constexpr auto EPSILON = static_cast<T>(0.00001);
 
-	template<typename T>
-	constexpr auto ZERO = static_cast<T>(0);
+			template<typename T>
+			constexpr auto ZERO = static_cast<T>(0);
 
-	template<typename T>
-	constexpr auto ONE = static_cast<T>(1);
+			template<typename T>
+			constexpr auto ONE = static_cast<T>(1);
 
-	template<typename T>
-	constexpr T to_radians(const T val) { return static_cast<T>((PI<double> / 180.0) * static_cast<double>(val)); }
+			template<typename T>
+			constexpr T to_radians(const T val) { return static_cast<T>((PI<double> / 180.0) * static_cast<double>(val)); }
 
-	template<typename T>
-	constexpr T to_degree(const T val) { return static_cast<T>((180.0 / PI<double>) * static_cast<double>(val)); }
+			template<typename T>
+			constexpr T to_degree(const T val) { return static_cast<T>((180.0 / PI<double>) * static_cast<double>(val)); }
+	}
+
 }
 #include <iostream>
 #ifndef NDEBUG
